a49d18870fe28f466afead2038d591aa1082a582
BrainStone <yannick@tedworld.de>
2024-03-13 03:17:51 +0100

    Move functions to separate file to avoid shadowing names

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
-import datetime
-import os
-from typing import Generator
 
-import pandas as pd
-from livesplit_parser import LivesplitData
+from funcs import *
 
 ##############
 # Settings
@@ -13,54 +9,9 @@ from livesplit_parser import LivesplitData
 SPLITS_DIR = '/home/yannick/LiveSplit/Splits/'
 VIDEOS_DIR = '/home/yannick/Videos/OBS/'
 
-
 ##############
-# Code
+# Main
 ##############
-
-
-def find_latest_record_file(search_path: str | os.PathLike) -> str | os.PathLike:
-    """Find the most recently modified file in a directory and its subdirectories."""
-    return max(
-        (os.path.join(dirpath, filename) for dirpath, dirnames, filenames in os.walk(search_path) for filename in
-         filenames), key=os.path.getmtime)
-
-
-def find_personal_best(lss_path: str | os.PathLike) -> pd.Series:
-    """Find the personal best record for a given .lss file."""
-    split_data = LivesplitData(lss_path)
-    all_runs: pd.DataFrame = split_data.attempt_info_df
-    completed_runs = all_runs.loc[all_runs['RunCompleted']]
-
-    if completed_runs.empty:
-        raise ValueError("No completed runs found")
-
-    return completed_runs.loc[completed_runs['RealTime_Sec'].idxmin()]
-
-
-def determine_timestamp_files(personal_best: pd.Series) -> list[str | os.PathLike]:
-    run_start: datetime.date = personal_best['started'].date()
-
-    candidate_days = [run_start, run_start + datetime.timedelta(days=1)]
-
-    candidate_file_names: Generator[str | os.PathLike, None, None] = (
-    os.path.join(VIDEOS_DIR, day.strftime('%Y-%m-%d.csv')) for day in candidate_days)
-    file_names: list[str | os.PathLike] = list(filter(os.path.isfile, candidate_file_names))
-
-    if not file_names:
-        raise ValueError("No timestamp files found")
-
-    return file_names
-
-
-def load_timestamps(timestamp_files: list[str | os.PathLike]) -> pd.DataFrame:
-    """Load timestamps from the specified timestamp files."""
-    timestamps = [pd.read_csv(file) for file in timestamp_files]
-
-    merged_timestamps = pd.concat(timestamps)
-    merged_timestamps.reset_index(drop=True, inplace=True)
-
-    return merged_timestamps
 
 
 if __name__ == "__main__":
@@ -70,7 +21,7 @@ if __name__ == "__main__":
     personal_best = find_personal_best(latest_record_file)
     print(f"Personal best: {personal_best}")
 
-    timestamp_files = determine_timestamp_files(personal_best)
+    timestamp_files = determine_timestamp_files(personal_best, VIDEOS_DIR)
     print(f"Timestamp files: {timestamp_files}")
 
     timestamps = load_timestamps(timestamp_files)
